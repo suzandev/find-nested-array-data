d5415597c3c8c0f38e7697c150468c61b6603f05
suzandev <dev.suzanchandra@gmail.com>
2024-01-30 18:34:46 +0600

    find nested array data

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Data from "./Components/Data/Data";
+
+function App() {
+  return (
+    <div className="container mx-auto text-center">
+      <h3 className="text-5xl font-bold">Hello World</h3>
+
+      <Data />
+    </div>
+  );
+}
+
+export default App;
